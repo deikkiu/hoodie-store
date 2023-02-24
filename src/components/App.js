1eb00c4e4058bd2527eab29fea36d2612bed076f
Timur Smagulov <deikku@mail.ru>
2023-02-25 01:18:48 +0600

    Feat: create dir for react components

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
