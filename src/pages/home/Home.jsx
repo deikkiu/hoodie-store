403a50620120350990e833f8288cc516d6fd2c8d
Timur Smagulov <deikku@mail.ru>
2023-03-10 23:11:13 +0600

    Feat: pages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from '../../components';
+import './Home.module.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      {/* Layout Screen */}
+      <Layout />
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from './home/Home';
+import Favorite from './favorite/Favorite';
+import Orders from './orders/Orders';
+import Shop from './shop/Shop';
+import ShopItem from './shop/ShopItem';
+import Error from './error/Error';
+
+export { Home, Favorite, Orders, Shop, ShopItem, Error };
